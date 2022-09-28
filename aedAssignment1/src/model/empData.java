e6abb1936abe9dc3010d9ce7bb6c4e466f4d5a87
RuchaChotaliaNEU <114604722+RuchaChotaliaNEU@users.noreply.github.com>
2022-09-29 04:10:40 +0530

    Add files via upload

---
+++ aedAssignment1/src/model/empData.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/** 
+ *
+ * @author Rucha Chotalia
+ */
+public class empData {
+    private ArrayList<employeeData> employee;
+    
+    public empData() {
+        this.employee= new ArrayList<employeeData>();
+        
+    }
+
+    public ArrayList<employeeData> getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(ArrayList<employeeData> employee) {
+        this.employee = employee;
+    }
+    
+}
---
+++ aedAssignment1/src/model/employeeData.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Rucha Chotalia
+ */
+public class employeeData {
+    private String name;
+    private int Idnumber;
+    private String Gender;
+    private int age;
+    private String level;
+    private String teamInfo;
+    private String postion;
+    private String email;
+    private int mobileno;
+    private Date currentDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIdnumber() {
+        return Idnumber;
+    }
+
+    public void setIdnumber(int Idnumber) {
+        this.Idnumber = Idnumber;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPostion() {
+        return postion;
+    }
+
+    public void setPostion(String postion) {
+        this.postion = postion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMobileno() {
+        return mobileno;
+    }
+
+    public void setMobileno(int mobileno) {
+        this.mobileno = mobileno;
+    }
+
+    public Date getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(Date currentDate) {
+        this.currentDate = currentDate;
+    }
+    
+    
+}
