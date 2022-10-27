12d8131235de9a2abbfe9ba8574eb03671b1cd9a
RuchaChotaliaNEU <114604722+RuchaChotaliaNEU@users.noreply.github.com>
2022-10-27 18:45:02 -0400

    Add files via upload

---
+++ Assignment2_HealthSystem/src/model/Patient.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ruchachotalia
+ */
+public class Patient {
+    private String patientID;
+    private String primaryDoctorName;
+    private String prefferedPharmacy;
+
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(String patientID) {
+        this.patientID = patientID;
+    }
+
+    public String getPrimaryDoctorName() {
+        return primaryDoctorName;
+    }
+
+    public void setPrimaryDoctorName(String primaryDoctorName) {
+        this.primaryDoctorName = primaryDoctorName;
+    }
+
+    public String getPrefferedPharmacy() {
+        return prefferedPharmacy;
+    }
+
+    public void setPrefferedPharmacy(String prefferedPharmacy) {
+        this.prefferedPharmacy = prefferedPharmacy;
+    }
+    
+  
+}
+
---
+++ Assignment2_HealthSystem/src/model/personDirectory.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ruchachotalia
+ */
+public class personDirectory {
+    private ArrayList<person> personDirectory;
+
+    public ArrayList<person> getPersonDirectory() {
+        return personDirectory;
+    }
+
+    public void setPersonDirectory(ArrayList<person> personDirectory) {
+        this.personDirectory = personDirectory;
+    }
+    
+}
